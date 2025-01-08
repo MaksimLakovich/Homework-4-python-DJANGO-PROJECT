7d776090a5651ad59b97983fd947ef608aa14ba1
Maksim Lakovich <maks.lakovich@gmail.com>
2025-01-08 23:32:28 +0300

    В приложении Catalog созданы и описаны модели Product и Category

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """Модель Category представляет категорию товаров в интернет-магазине."""
+    category_name = models.CharField(max_length=100, verbose_name="Наименование категории", help_text="Введите название категории", unique=True, blank=False)
+    description = models.TextField(verbose_name="Описание категории", help_text="Введите описание категории", blank=True)
+
+    def __str__(self):
+        """Метод определяет строковое представление объекта. Полезно для отображения объектов в админке/консоли."""
+        return f"{self.category_name}"
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ["category_name"]
+        db_table = "catalog_categories"
+
+
+class Product(models.Model):
+    """Модель Product представляет товар в интернет-магазине."""
+    product_name = models.CharField(max_length=100, verbose_name="Наименование товара", help_text="Введите название товара", unique=True, blank=False)
+    description = models.TextField(verbose_name="Описание товара", help_text="Введите описание товара", blank=True)
+    image = models.ImageField(upload_to='product_photo/', verbose_name='Фотография товара', help_text="Загрузите фото товара", null=True)
+    category = models.ForeignKey(to=Category, on_delete=models.CASCADE, related_name="products", help_text="Выберите категорию", null=True)
+    price = models.IntegerField(help_text="Укажите цену товара", blank=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """Метод определяет строковое представление объекта. Полезно для отображения объектов в админке/консоли."""
+        return f"{self.product_name}"
+
+    class Meta:
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
+        ordering = ["product_name"]
+        db_table = "catalog_products"
--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # namespace='catalog' это заданное пространство имен, которое есть в catalog/urls.py с помощью CatalogConfig.name
     path('catalog/', include('catalog.urls', namespace='catalog')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
